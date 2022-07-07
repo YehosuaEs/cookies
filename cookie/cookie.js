5ca679b5c230347eeb60eea3a12613781f8a1521
YehosuaEs <yehosuaes@gmail.com>
2022-07-07 04:08:55 +0200

    Js file with the functionality of the cookiPopup to preserve the accept_cookies in to localStorage

--- cookie/cookie.js
+++ cookie/cookie.js
@@ -1,5 +1,19 @@
-/* ------ CLOSE MODAL FUNCTION ------ */
-function cerrar() {
-    document.getElementById('modal').innerHTML;
-    modal.style.display = 'none';
+// Close entire cookiePopup with out accept
+function closePopup() {
+    document.getElementById('cookiePopup').style.display = 'none'; 
 }
+
+// Conditional to check if there is info inside local storage
+if (!localStorage.getItem('accept_cookies')){
+    document.getElementById("cookiePopup").style.display = "flex" 
+} else (
+    document.getElementById("cookiePopup").style.display = "none"   
+)
+
+// Set cookie consent and close the cookiePopup
+function acceptCookieConsent() {
+    document.getElementById("cookiePopup").style.display = "none"
+    localStorage.setItem('accept_cookies', true)  
+}
+ 
+  
